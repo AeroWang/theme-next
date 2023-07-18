41847372794c14e8c3664a51fc0d1764ae229078
aero <44799877+AeroWang@users.noreply.github.com>
2023-07-18 22:38:47 +0800

    fix: The hover color of the article title in the latest article card is invalid in dark mode

--- app/(blog)/_components/Aside/LatestPostList.tsx
+++ app/(blog)/_components/Aside/LatestPostList.tsx
@@ -30,7 +30,7 @@ const List = async () => {
           title={articleItem.spec?.title}
           key={articleItem.metadata.name}
           href={`/p/${articleItem.spec?.slug}`}
-          className={'mb-4 block text-sm text-black last:mb-0 hover:text-primary dark:text-white-85 md:text-base'}>
+          className={'mb-4 block text-sm text-black last:mb-0 hover:!text-primary dark:text-white-85 md:text-base'}>
           <article className={'line-clamp-1'}>{articleItem.spec?.title}</article>
         </Link>
       ))}
