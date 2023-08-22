57a0f3c30a9425560dcd5894daf97f7c626c8862
Aero <44799877+AeroWang@users.noreply.github.com>
2023-08-22 21:37:49 +0800

    chore: update robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -1,14 +1,20 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
+  // NOTE: 演示版阻止抓取
   return {
     rules: [
       {
         userAgent: '*',
         allow: '/',
-        disallow: '/confirm/',
+        disallow: '/private/',
       },
+      /*      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/confirm/',
+      },*/
     ],
-    sitemap: `https://next.aerowang.cn/sitemap.xml`,
+    // sitemap: `https://next.aerowang.cn/sitemap.xml`,
   }
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,21 +2,18 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   // TODO: dynamic generate
+  // NOTE: 演示版不做实际有效 sitemap.xml
   return [
     {
-      url: 'https://next.aerowang.cn',
+      url: 'https://acme.com',
       lastModified: new Date(),
     },
     {
-      url: 'https://next.aerowang.cn/about',
+      url: 'https://acme.com/about',
       lastModified: new Date(),
     },
     {
-      url: 'https://next.aerowang.cn/links',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://next.aerowang.cn/journals',
+      url: 'https://acme.com/blog',
       lastModified: new Date(),
     },
   ]
